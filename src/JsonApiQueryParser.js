c19d8bc1e2ed8abaebc5c4180a0eaadd93142af1
Alec Hirsch <alec.hirsch@anchorops.com>
2016-09-06 16:11:19 -0400

    Added support for filtering by type: like, not, lt, lte, gt, gte

--- src/JsonApiQueryParser.js
+++ src/JsonApiQueryParser.js
@@ -8,7 +8,8 @@ let PARSE_PARAM = Object.freeze({
   parseFields: /^fields\[(.*?)\]\=.*?$/i,
   parsePage: /^page\[(.*?)\]\=.*?$/i,
   parseSort: /^sort\=(.*?)/i,
-  parseFilter: /^filter\[(.*?)\]\=.*?$/i
+  parseFilter: /^filter\[(.*?)\]\=.*?$/i,
+  parseFilterType: /^filterType\[(.*?)\]\[(.*?)\]\=.*?$/i
 });
 
 
@@ -32,7 +33,15 @@ class JsonApiQueryParser {
         fields: {},
         sort: [],
         page: {},
-        filter: {}
+        filter: {},
+        filterType: {
+          like: {},
+          not: {},
+          lt: {},
+          lte: {},
+          gt: {},
+          gte: {}
+        }
       }
     };
 
@@ -223,6 +232,40 @@ class JsonApiQueryParser {
     });
 
     requestDataSubset.filter[targetColumn] = targetFilterString;
+
+    return requestDataSubset;
+  }
+
+  /**
+   * [Note: The are no proper specifications for this parameter yet.
+   * For now the filter is implemented similar to the fields parameter. Values should be url encoded to allow for special characters.]
+   *
+   * @param {[string]} filterString [Required sort query string piece. Example: "filter[like][name]=John%20Doe".]
+   * @param {[object]} requestDataSubset [Required reference to the requestData.queryData object.]
+   * @return {[object]} requestDataSubset [Returning the modified request data.]
+   *
+   **/
+  static parseFilterType (filterString, requestDataSubset) {
+    let targetType;
+    let targetColumn;
+    let targetFilterString;
+    let filterNameRegex = /^filter.*?\=(.*?)$/i;
+
+    targetType = filterString.replace(PARSE_PARAM.parseFilterType, function(match, $1, $2, $3) {
+      return $1;
+    });
+
+    targetColumn = filterString.replace(PARSE_PARAM.parseFilterType, function(match, $1, $2, $3) {
+      return $2;
+    });
+
+    targetFilterString = filterString.replace(filterNameRegex, function(match, $1, $2, $3) {
+      return $1;
+    });
+
+    if(requestDataSubset.filterType[targetType]){
+      requestDataSubset.filterType[targetType][targetColumn] = targetFilterString;
+    }
 
     return requestDataSubset;
   }
